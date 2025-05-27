6489c44d56db76d0d187c3b358ea82638af188a9
DQTRUNGDQ <trungthpthy@gmail.com>
2025-05-27 10:31:34 +0700

    update

--- server/src/config/dist/firebaseConfig.js
+++ server/src/config/dist/firebaseConfig.js
@@ -1,11 +1,8 @@
-"use strict";
-exports.__esModule = true;
-exports.bucket = void 0;
-var firebase_admin_1 = require("firebase-admin");
-var firebase_adminsdk_json_1 = require("../../firebase-adminsdk.json");
-firebase_admin_1["default"].initializeApp({
-    credential: firebase_admin_1["default"].credential.cert(firebase_adminsdk_json_1["default"]),
-    storageBucket: "social-media-e435b.firebasestorage.app"
-});
-var bucket = firebase_admin_1["default"].storage().bucket();
-exports.bucket = bucket;
+// import admin, { ServiceAccount } from "firebase-admin";
+// import serviceAccount from "../../firebase-adminsdk.json";
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount as ServiceAccount),
+//   storageBucket: "social-media-e435b.firebasestorage.app",
+// });
+// const bucket = admin.storage().bucket();
+// export { bucket };
